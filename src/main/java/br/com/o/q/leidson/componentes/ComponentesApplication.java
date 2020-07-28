0195b81ed50eb9d6dc58c221a988313ef4943cf4
Leidson Oliveira <o.q.leidson@gmail.com>
2020-07-27 21:07:28 -0300

    Projeto criado

---
+++ src/main/java/br/com/o/q/leidson/componentes/ComponentesApplication.java
@@ -0,0 +1,13 @@
+package br.com.o.q.leidson.componentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/o/q/leidson/componentes/ComponentesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.o.q.leidson.componentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
