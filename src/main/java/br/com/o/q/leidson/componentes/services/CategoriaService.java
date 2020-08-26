c4847222a80c3980cb4af211729d675d2ec3eff7
Leidson Oliveira <o.q.leidson@gmail.com>
2020-08-26 20:41:08 -0300

    Atualizando e deletando uma Categoria

--- src/main/java/br/com/o/q/leidson/componentes/services/CategoriaService.java
+++ src/main/java/br/com/o/q/leidson/componentes/services/CategoriaService.java
@@ -3,27 +3,45 @@ package br.com.o.q.leidson.componentes.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.o.q.leidson.componentes.domain.Categoria;
 import br.com.o.q.leidson.componentes.repositories.CategoriaRepository;
+import br.com.o.q.leidson.componentes.services.exceptions.DataIntegrityException;
+import br.com.o.q.leidson.componentes.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new br.com.o.q.leidson.componentes.services.exceptions.ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
-	
-	public Categoria insert (Categoria obj) {
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	
+
+	public Categoria update(Categoria obj) {
+		// chama o método find e lança uma exceção caso o id não existaf
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete (Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui componentes");
+			
+		}
+	}
+
 }
